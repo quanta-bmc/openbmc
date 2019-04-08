d94520b704b763d8c3e621439b4a603b6687bdd8
Samuel Jiang <Samuel.Jiang@quantatw.com>
2019-04-08 21:33:21 +0800

    meta-gsj: fix polling powergood and recovery
    fix polling timer issue for recovery function
    
    Signed-off-by: Samuel Jiang <Samuel.Jiang@quantatw.com>

--- recipes-phosphor/quanta-nvme-powerctrl/files/nvme_powermanager.sh
+++ recipes-phosphor/quanta-nvme-powerctrl/files/nvme_powermanager.sh
@@ -15,23 +15,55 @@ function read_present_set_related_power(){
     #$1 read present number, $2 output power gpio
     var="${U2_PRESENT_STATUS[$1]}"
     # present 0 is plugged,present 1 is removal
-    if [ "$var" == "0" ];then
+    echo "U2 $i present is ${var}"
+    if [ "$var" -eq 0 ];then
+        set_gpio_direction $2 "high"
+        sleep 0.2
+        set_gpio_direction $2 "low"
+        sleep 0.2
         set_gpio_direction $2 "high"
     else 
         set_gpio_direction $2 "low"
     fi
+    sleep 0.2
+    echo "U2 set power is $(cat /sys/class/gpio/gpio$2/value)"
 }
 
 function update_u2_status(){
-  #$1 read present gpio
+  #$1 read present gpio $2 power gpio
   var=$(cat /sys/class/gpio/gpio$2/value)
   U2_PRESENT_STATUS[$1]="$var"
+ }
+
+function update_u2_direct(){
+  U2_PRESENT_STATUS[$1]="$2"
+}
+
+function recovery_pwrgd(){
+    present=$2
+    pwrgd=$(cat /sys/class/gpio/gpio$3/value)
+    if [ "$present" -eq "$pwrgd" ];then
+        set_gpio_direction "${POWER_U2[$1]}" "low"
+        sleep 0.2
+        set_gpio_direction "${POWER_U2[$1]}" "high"
+        sleep 0.5
+        pwrgd=$(cat /sys/class/gpio/gpio$3/value)
+        if [ "$present" -eq "$pwrgd" ];then
+            #set fault led
+            busctl set-property xyz.openbmc_project.LED.GroupManager /xyz/openbmc_project/led/groups/led\_u2\_$1\_fault xyz.openbmc_project.Led.Group Asserted b true
+        else
+            busctl set-property xyz.openbmc_project.LED.GroupManager /xyz/openbmc_project/led/groups/led\_u2\_$1\_fault xyz.openbmc_project.Led.Group Asserted b false
+        fi
+    fi
 }
 
 function check_present_and_powergood(){
     #$2 present gpio, $3 powergood gpio
-    present=$(cat /sys/class/gpio/gpio$2/value)
+    #present=$(cat /sys/class/gpio/gpio$2/value)
+    present=$2
+    sleep 0.5
     pwrgd=$(cat /sys/class/gpio/gpio$3/value)
+    echo "U2 $i present is ${present} and powergood is ${pwrgd}"
     path=`expr $1`
     if [ "$present" -eq 0 ] && [ "$pwrgd" -eq 1 ];then        
         busctl set-property xyz.openbmc_project.nvme.manager /xyz/openbmc_project/nvme/$path xyz.openbmc_project.Inventory.Item Present b true
@@ -52,7 +84,7 @@ function check_present_and_powergood(){
 ##Initial U2 present status
 for i in {0..7};
 do 
-    update_u2_status $i "${U2_PRESENT[$i]}"
+    update_u2_status $i ${U2_PRESENT[$i]}
 done
 
 
@@ -64,20 +96,18 @@ do
     ## 1 scend scan all loop
     sleep 0.125
     read=$(cat /sys/class/gpio/gpio${U2_PRESENT[$i]}/value)
-    if [ "${U2_PRESENT_STATUS[$1]}" != read ];then
-        update_u2_status $i "${U2_PRESENT[$i]}"
+    if [ $read -eq 0 ];then 
+        recovery_pwrgd $i $read "${PWRGD_U2[$i]}"
+    fi
+    if [ "${U2_PRESENT_STATUS[$i]}" != "$read" ];then
+        echo "Update present status"
+        update_u2_direct $i "$read"
         read_present_set_related_power $i "${POWER_U2[$i]}"
-        check_present_and_powergood $i "${U2_PRESENT[$i]}" "${PWRGD_U2[$i]}"
+        #check_present_and_powergood $i "${U2_PRESENT[$i]}" "${PWRGD_U2[$i]}"
+        check_present_and_powergood $i $read "${PWRGD_U2[$i]}"
     fi 
   done
 done
 
-
-##Echo U2 present status
-# for i in {0..7};
-# do 
-#     echo "${U2_PRESENT_STATUS[$i]}"
-# done
-  
 
 
