121f8fb6626dd768f8fb215d1222d20f56ce7098
Duke Du <Duke.Du@quantatw.com>
2019-05-17 14:46:34 +0800

    Get hwmon path automatically and add comment

--- recipes-phosphor/detect-fan-fail/files/detect-fan-fail.sh
+++ recipes-phosphor/detect-fan-fail/files/detect-fan-fail.sh
@@ -1,31 +1,53 @@
 #!/bin/bash
 
+# get fan threshold by d-bus command
 function get_threshold_value() {
-  #$1 gpio pin
     get_property_path='xyz.openbmc_project.Sensor.Threshold.Critical CriticalLow'
     # echo "busctl get-property $1 $2 $get_property_path"
     threshold_value="$(busctl get-property $1 $2 $get_property_path | awk '{print $2}')"
     echo "$threshold_value"
 }
 
+# get fan rpm by d-bus command
 function get_fan_value() {
-  #$1 gpio pin
     get_property_path='xyz.openbmc_project.Sensor.Value Value'
     # echo "busctl get-property $1 $2 $get_property_path"
     fan_value="$(busctl get-property $1 $2 $get_property_path | awk '{print $2}')"
     echo "$fan_value"
 }
 
+# set fan pwm by d-bus command
 function set_fan_value() {
-  #$1 gpio pin
     set_property_path='xyz.openbmc_project.Control.FanPwm'
     # echo " busctl set-property $1 $2 $set_property_path Target t 100 "
     busctl set-property $1 $2 $set_property_path Target t 255
 }
 
-echo "================Start Fan Detect: ==============="
+#find hwmon path of fan
+function find_hwmon_path() {
+    # calculate number of hwmons
+    hwmon_num="$(ls /sys/class/hwmon/ | awk '{count+= 1} END {print count}')"
+    for i in $(seq 1 $hwmon_num); 
+    do 
+        # echo $i 
+        #combine number + p
+        i+="p"
+        #get path of hwmon
+        path="$(busctl --no-pager | grep Hwmon[0-9] | awk '{print $1}' | sed -n "$i")" 
+        # echo "$path"
+        #perform busctl tree to show content of each path of hwmon, and grep fan_tach
+        busctl_tree="$(busctl tree $path --no-pager | grep fan_tach)"
+        # echo $busctl_tree
+        # $? is used to record last command state
+        if [ $? -eq 0 ];then
+            echo "$path"
+            break
+            # echo "Find"
+        fi
+    done
+}
 
-hwmon_path='xyz.openbmc_project.Hwmon-377141964.Hwmon1'
+echo "================Start Fan Detect: ==============="
 fan_tach_path=( '/xyz/openbmc_project/sensors/fan_tach/Fan0_0_RPM'
                 '/xyz/openbmc_project/sensors/fan_tach/Fan0_1_RPM'
                 '/xyz/openbmc_project/sensors/fan_tach/Fan1_0_RPM'
@@ -34,11 +56,10 @@ fan_tach_path=( '/xyz/openbmc_project/sensors/fan_tach/Fan0_0_RPM'
                 '/xyz/openbmc_project/sensors/fan_tach/Fan2_1_RPM'
                 )
 
+hwmon_path=$(find_hwmon_path)
 critical_threshold=$(get_threshold_value $hwmon_path ${fan_tach_path[0]})
 # echo "$critical_threshold"
-
 check_fail_flag=0
-set_count=0
 current_fan_value=()
 while true; do
     sleep 2
@@ -61,22 +82,19 @@ while true; do
                     # echo "j: $j , i: $i , i-1: $(($i-1)) , j%2: $(($j%2))"
                     # echo "${fan_tach_path[$j]}"
                     set_fan_value $hwmon_path ${fan_tach_path[$j]}
-                    set_count=$(($set_count+1))
                     check_fail_flag=1
-                    set_count=0
                 fi
             done
             current_fan_value=()
             break
         fi
         # echo 'Fans are going to normal'
-
         if [ $i -eq $((${#fan_tach_path[@]}-1)) ] && [ $check_fail_flag -eq 1 ]; then
            check_fail_flag=0
            systemctl restart phosphor-pid-control
            echo "Fans are going to normal"
-           sleep 15
+        # debug for late code run 
+        #    sleep 15 
         fi
     done
-    
 done
