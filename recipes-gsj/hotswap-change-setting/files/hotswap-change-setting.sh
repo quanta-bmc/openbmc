7f1debdeab964b05e46250402639924859925c6a
SpencerKu <Spencer.Ku@quantatw.com>
2019-12-02 15:08:51 +0800

    meta-quanta: gsj: Modify setting hotswap controller service.
    (1) modify conditional judgment for setting hotswap register
    (2) Add GPIO folder checker

--- recipes-gsj/hotswap-change-setting/files/hotswap-change-setting.sh
+++ recipes-gsj/hotswap-change-setting/files/hotswap-change-setting.sh
@@ -2,8 +2,10 @@
 
 I2C_BUS=11
 CHIP_ADDR=0x15
+GPIO_ID=91
+GPIO_BasePath=/sys/class/gpio
 
-function set_hotswap()
+function set_hotswap_reg()
 {
     #set reg "0xd9" bit 3 to 1
     i2cset -f -y $I2C_BUS $CHIP_ADDR 0xd9 0x08
@@ -15,21 +17,48 @@ function get_hotswap_value()
     echo "$(i2cget -f -y $I2C_BUS $CHIP_ADDR 0xd9)"
 }
 
-echo "setting hotswap controller..."
-set_hotswap
-
-for i in {0..3};
-do
-    if [ "$i" == "3" ];then
-        echo "change hotswap controller setting failed after retry 3 times."
+function export_gpio()
+{
+    if [ -d "$GPIO_BasePath/gpio$GPIO_ID" ]; then
+        echo "gpio$GPIO_ID folder exist, skip export."
     else
-        hotswap_value=$(get_hotswap_value)
-        echo "get hotswap controller return value : $hotswap_value"
-        if [ "$hotswap_value" == "0x08" ];then
-            echo "change hotswap controller setting success."
-            break;
+        echo "Export gpio$GPIO_ID..."
+        echo $GPIO_ID > $GPIO_BasePath/export
+    fi   
+}
+
+function get_gpio_value()
+{
+    echo "$(cat $GPIO_BasePath/gpio$GPIO_ID/value)"
+}
+
+function setting_hotswap()
+{
+    echo "setting hotswap controller..."
+    set_hotswap_reg
+
+    for i in {0..3};
+    do
+        if [ "$i" == "3" ];then
+            echo "change hotswap controller setting failed after retry 3 times."
         else
-            echo "hotswap controller setting failed, retry $i times..."
+            hotswap_value=$(get_hotswap_value)
+            echo "get hotswap controller return value : $hotswap_value"
+            if [ "$hotswap_value" == "0x08" ];then
+                echo "change hotswap controller setting success."
+                break;
+            else
+                echo "hotswap controller setting failed, retry $i times..."
+            fi
         fi
-    fi
-done
+    done    
+}
+
+export_gpio
+gpio_value=$(get_gpio_value)
+if [ "$gpio_value" == "1" ];then
+    echo "gpio$GPIO_ID value is: $gpio_value, setting hotswap."
+    setting_hotswap
+else
+    echo "gpio$GPIO_ID value is: $gpio_value, no need to set hotswap."
+fi
